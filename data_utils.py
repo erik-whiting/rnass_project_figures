1eb11ef68edc1c8fd2efc4bc4a3541936a7b5472
erik-whiting <erik@erikwhiting.com>
2026-04-20 09:31:33 -0500

    Add utils script

---
+++ data_utils.py
@@ -0,0 +1,15 @@
+models = [
+    "ContextFold",
+    "ContraFold",
+    "EternaFold",
+    "IPKnot",
+    "MXFold",
+    "MXFold2",
+    "NeuralFold",
+    "NUPACK",
+    "pKnots",
+    "RNAFold",
+    "RNAStructure",
+    "Simfold",
+    "SPOT-RNA"
+]
