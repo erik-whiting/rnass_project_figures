92866e034638ef3669b3a829595d334972dec1a3
erik-whiting <erik@erikwhiting.com>
2026-04-20 09:57:06 -0500

    Make hard vs easy plots

--- data_utils.py
+++ data_utils.py
@@ -13,3 +13,31 @@ models = [
     "Simfold",
     "SPOT-RNA"
 ]
+
+pretty_attr_map = {
+    'sequence_length': 'Sequence Length',
+    'gc_content': 'GC-Content',
+    'sequence_entropy': 'Sequence Entropy',
+    'average_longest_gc_helix': 'Longest GC Helix',
+    'mfe': 'MFE',
+    'ens_def': 'Ensemble Defect',
+    'longest_consecutive_A': 'Longest Conesecutive A',
+    'longest_consecutive_C': 'Longest Conesecutive C',
+    'longest_consecutive_G': 'Longest Conesecutive G',
+    'longest_consecutive_U': 'Longest Conesecutive U',
+    'avg_gu_pairs': 'GU Pairs',
+    'averge_bps_predicted': 'Average Basepairs Predicted',
+    'hairpin_count': 'Hairpin Count',
+    'helix_count': 'Helix Count',
+    'single_strand_count': 'Single Strand Count',
+    'junction_count': 'Junction Count',
+    'average_mway_count': 'Multiway Junction Count',
+    'longest_singlestrand': 'Longest Single Strand',
+    'average_singlestrand_size': 'Average Single Strand Size',
+    'longest_helix': 'Longest Helix',
+    'average_helix_size': 'Average Helix Size',
+    'average_hamming_distance_of_preds': 'Average Hamming Distance Between Predictions',
+    'average_au_pairs_of_helices': 'Average AU Pairs in Helices',
+    'helices_with_reverse_compliment': 'Helices with Reverse Complements',
+    'rate_of_gt_4_unpaired_nt_in_hairpin': 'Rate of >4 Unpaired Nucleotides in Hairpin'
+}
